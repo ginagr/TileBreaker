148d7fe7988d9742b409b097601a6aee3ab78ba2
ggr <ginagonzalezroundey@gmail.com>
2018-01-03 21:53:10 -0500

    tutorial

---
+++ TileBreaker/TutorialController.swift
@@ -0,0 +1,22 @@
+//
+//  TutorialController.swift
+//  TileBreaker
+//
+//  Created by GGR on 1/2/18.
+//  Copyright © 2018 ggr. All rights reserved.
+//
+
+import UIKit
+
+class TutorialController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.isScrollEnabled = true
+        scrollView.contentSize.height = UIScreen.main.bounds.height
+    }
+}
